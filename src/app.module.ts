7a93b919a55712299c49d86599882fc755879f4f
IvanKlimkin <54597839+iKlimkin@users.noreply.github.com>
2024-01-09 19:25:19 +0300

    to deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import settings from 'src/infra/settings/app.settings';
 import { controllers } from './settings/app-controllers';
 import { providers } from './settings/app-providers';
 import { mongooseSchemas } from './settings/mongoose-schemas';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(settings.MONGO_URL),
     MongooseModule.forFeature(mongooseSchemas),
   ],
