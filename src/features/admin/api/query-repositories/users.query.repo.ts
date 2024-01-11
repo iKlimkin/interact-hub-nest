28f00e1750a593b835df38d5b28e5a6622b87a53
IvanKlimkin <54597839+iKlimkin@users.noreply.github.com>
2024-01-11 18:31:20 +0300

    refactoring fabrics blog, post, user, comment refactoring user.query.repo add functional to pagination

---
+++ src/features/admin/api/query-repositories/users.query.repo.ts
@@ -0,0 +1,75 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { SortingQueryModel } from 'src/infra/SortingQueryModel';
+import { PaginationViewModel } from 'src/infra/paginationViewModel';
+import { getPagination } from 'src/infra/utils/pagination';
+import { getSearchTerm } from 'src/infra/utils/searchTerm';
+import {
+  UserAccount,
+  UserAccountModelType,
+} from '../../domain/entities/userAccount.schema';
+import { getUserViewModel } from '../models/userAdmin.view.models/getUserAdmin.vie.model';
+import { UserViewModel } from '../models/userAdmin.view.models/userAdmin.view.model';
+
+@Injectable()
+export class UsersQueryRepository {
+  constructor(
+    @InjectModel(UserAccount.name)
+    private UserAccountModel: UserAccountModelType,
+  ) {}
+
+  async getAllUsers(
+    inputData: SortingQueryModel,
+  ): Promise<PaginationViewModel<UserViewModel>> {
+    const filter = getSearchTerm(
+      {
+        searchEmailTerm: inputData.searchEmailTerm,
+        searchLoginTerm: inputData.searchLoginTerm,
+      },
+      !0,
+    );
+
+    const { pageNumber, pageSize, skip, sort } = await getPagination(
+      inputData,
+      !0,
+    );
+
+    try {
+      const users = await this.UserAccountModel.find(filter)
+        .sort(sort)
+        .skip(skip)
+        .limit(pageSize);
+
+      const totalCount = await this.UserAccountModel.countDocuments(filter);
+      const pagesCount = Math.ceil(totalCount / pageSize);
+
+      return {
+        pagesCount,
+        page: pageNumber,
+        pageSize,
+        totalCount,
+        items: users.map(getUserViewModel),
+      };
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Database fails operate with find users',
+        error,
+      );
+    }
+  }
+
+  async getUserById(userId: string): Promise<UserViewModel | null> {
+    try {
+      const user = await this.UserAccountModel.findById(userId);
+
+      if (!user) return null;
+
+      return getUserViewModel(user);
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Database fails operate with find user',
+        error,
+      );
+    }
+  }
+}
